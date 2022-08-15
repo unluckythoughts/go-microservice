9a44b8c86f7f40a7b82bc5df982673a385e356bb
unluckythoughts <gvsmraju@gmail.com>
2022-08-14 23:28:36 -0400

    setting up cors headers

--- tools/web/router.go
+++ tools/web/router.go
@@ -19,6 +19,7 @@ type (
 	router struct {
 		_int        *httprouter.Router
 		l           *zap.Logger
+		cors        bool
 		middlewares []Middleware
 	}
 )
@@ -57,6 +58,7 @@ func newRouter(l *zap.Logger, enableCors bool) *router {
 	r := &router{
 		_int: httprouter.New(),
 		l:    l,
+		cors: enableCors,
 	}
 
 	r.attachBasicHandlers(enableCors)
@@ -129,6 +131,10 @@ func (r *router) routerHandler(handlers []interface{}) httprouter.Handle {
 	}
 
 	return httprouter.Handle(func(w http.ResponseWriter, httpReq *http.Request, p httprouter.Params) {
+		if r.cors {
+			setCORSHeaders(w)
+		}
+
 		req := r.newRequest(httpReq, p)
 		resp := &response{request: req, respWriter: w}
 
